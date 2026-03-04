0add46b8590932b5c0c0b1061fe320a9db4f9f05
Christopher Kenny <ctk44@cornell.edu>
2026-03-04 18:39:46 -0500

    regex changes

--- tests/testthat/helper-snapshots.R
+++ tests/testthat/helper-snapshots.R
@@ -1,17 +1,9 @@
 # Normalize temp directory paths in snapshots to make them reproducible
 # across different runs and platforms.
 normalize_snap_paths <- function(x) {
-  # Replace any path segment containing a temp dir pattern
-  x <- gsub(
-    "'[A-Za-z:/\\\\]*[Tt]emp[A-Za-z0-9:/\\\\._-]+'",
-    "'<path>'",
-    x
-  )
-  # Also handle backtick-quoted paths
-  x <- gsub(
-    '`[A-Za-z:/\\\\]*[Tt]emp[A-Za-z0-9:/\\\\._-]+`',
-    '`<path>`',
-    x
-  )
+  # Unix absolute paths in single quotes
+  x <- gsub("'/[^']*'", "'<path>'", x)
+  # Windows absolute paths in single quotes
+  x <- gsub("'[A-Za-z]:[^']*'", "'<path>'", x)
   x
 }
